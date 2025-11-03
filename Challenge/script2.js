c57a45e4baf3ae23ee30c63c7eb6cfa82cbacbee
Samantha Anderson <samirulu87@gmail.com>
2025-11-03 08:34:46 -0700

    Add files via upload

---
+++ Challenge/script2.js
@@ -0,0 +1,38 @@
+
+
+const name = "LooLoo";/*This varable is a string, so it uses quotation marks*/
+const userage = 32; /*This varable is a number, so it does not need any marks and it just needs a real number*/
+const studentornot = false; /*This varable is a boolean so it does not any marks, just a true or false*/
+const favecolor = null; /*This varable has not been chosent to be defined, so add a null to nullafiy the varable*/
+
+
+const message = `Hi, my name is ${name} and I am ${userage} years old. Am I a student? ${studentornot}`
+/* The const message includes the main sentince then adds the needed varables by using the money sign and curly braces.*/
+ console.log(message);
+/* The console log is where the predefined message is displayed.*/
+
+
+
+const playername = "Player200745";/*This varable is a string, so it uses quotation marks*/
+let points = 50;/*This varable is a number, so it does not need any marks it may also change so a let is used instead of a const*/
+let over = false; /*This varable is a boolean so it does not any marks, just a true or false, it also may change over time so the const it changed to a let instead*/
+const playermessage = `${playername} finished the game with ${points}. Game over? ${over}`;
+/* Again thnis line includes the main sentince then adds the needed varables by using the money sign and curly braces from the predefined varables here.*/
+console.log(playermessage);
+/* The console log is where the message is displayed.*/
+
+points= 100; /*Changed varable points here to display a new score*/
+over= true; /*Changed varable over here to change if the true or false is dislayed*/
+console.log(`End Points: ${points}`);/*Displayed the new varable content here*/
+console.log(`Game over? ${over}`);/*Displayed the new varable content here*/
+
+
+
+const movie = "Barbie";/*This varable is a string, so it uses quotation marks*/
+const watched = 3;  /*This varable is a number, so it does not need any marks and it just needs a real number*/
+const likedornot = true;/*This varable is a boolean so it does not any marks, just a true or false*/
+const rating = null; /*This varable has not been chosent to be defined, so add a null to nullafiy the varable*/
+const moviemessage = `i watched ${movie} ${watched} times. Did I like it? ${likedornot}.`
+/* Again thnis line includes the main sentince then adds the needed varables by using the money sign and curly braces from the predefined varables here.*/
+console.log(moviemessage)
+/* Final console log display.*/
